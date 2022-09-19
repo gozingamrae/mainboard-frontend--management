27e2aa9ac644853146e18e67727ea9a218e31906
jihi32123 <jihi32123@gmail.com>
2022-09-19 13:44:48 +0900

    Refactor inventory.js 스타일 추가

--- src/inventory/Inventory.js
+++ src/inventory/Inventory.js
@@ -1,14 +1,19 @@
 import style from "./css/Inventory.module.css"
 
 function Inventory(){
-    const inventorys= [{"sirialNo" : "1", "category":"스플랜더" , "status": "창고" , "대여자" : "jihi1013"},
-                        {"sirialNo" : "2", "category":"스플랜더" , "status": "창고" , "대여자" : "jihi1013"},
-                        {"sirialNo" : "3", "category":"부루마블" , "status": "창고" , "대여자" : "jihi1013"}]
+    const inventorys= [{"sirialNo" : "1", "category":"스플랜더" , "status": "창고" , "등급": "상","대여자" : ""},
+                        {"sirialNo" : "2", "category":"스플랜더" , "status": "대여중" , "등급": "상", "대여자" : "jihi1013"},
+                        {"sirialNo" : "3", "category":"부루마블" , "status": "검수중" , "등급": "상" , "대여자" : ""},
+                        {"sirialNo" : "4", "category":"할리갈리" , "status": "창고" , "등급": "상", "대여자" : ""},
+                        {"sirialNo" : "5", "category":"부루마블" , "status": "창고" , "등급": "상", "대여자" : ""},
+                        {"sirialNo" : "5", "category":"부루마블" , "status": "창고" , "등급": "상", "대여자" : ""},
+                        {"sirialNo" : "5", "category":"부루마블" , "status": "창고" ,  "등급": "상", "대여자" : ""},
+                        {"sirialNo" : "5", "category":"부루마블" , "status": "대여중" , "등급": "상", "대여자" : "jihi1013"}]
 
     return (
         <div className={style.inventoryBox}>
             <div className={style.title}>
-                재고 조회 / 수정
+                재고 조회/수정
             </div>
             <div className={style.box}>
                 <div>
@@ -44,10 +49,10 @@ function Inventory(){
                 <button>검색</button>
             </div>
             <div className={style.box}>
-                <table>
-                    <th><input type="checkbox"/></th><th>재고코드</th><th>카테고리명</th><th>상태</th><th>대여자명</th>
+                <table className={style.inventoryTable}>
+                    <th><input type="checkbox"/></th><th>재고코드</th><th>카테고리명</th><th>등급</th><th>상태</th><th>대여자명</th>
                     {inventorys.map((inventory)=>(
-                        <tr><th><input type="checkbox"/></th><td>{inventory.sirialNo}</td><td>{inventory.category}</td><td>{inventory.status}</td><td>{inventory.대여자}</td></tr>))}
+                        <tr><td><input type="checkbox"/></td><td>{inventory.sirialNo}</td><td>{inventory.category}</td><td>{inventory.등급}</td><td>{inventory.status}</td><td>{inventory.대여자}</td></tr>))}
                 </table>
             </div>
         </div>
