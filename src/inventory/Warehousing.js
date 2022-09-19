ff5a689d77d4ec60293354f25977d3a73d8f8cdc
jihi32123 <jihi32123@gmail.com>
2022-09-19 20:55:27 +0900

    Add 입고 페이지 추가

---
+++ src/inventory/Warehousing.js
@@ -0,0 +1,12 @@
+import style from "./css/Inventory.module.css"
+
+function Warehousing(){
+    
+    return (
+        <div className={style.inventoryBox}>
+            <h1>입고</h1>
+        </div>
+    )
+}
+
+export default Warehousing;
