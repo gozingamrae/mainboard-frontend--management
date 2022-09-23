e440b33e85edda69247a4334073bb16b74239562
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-09-23 21:56:20 +0900

    Add 블랙리스트 관리 페이지

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import CuponReport from "./cupon/pages/cuponreport";
 import Inspect from "./inspect/Inspect";
 import Common from "./common/Common";
 import Member from "./member/pages/Member";
+import BlackList from "./member/pages/Blacklist";
 import "./App.module.css";
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route path="/cuponreport" element={<CuponReport/>} />
           <Route path="/inspect" element={<Inspect/>} />
           <Route path="/member" element={<Member/>} />
+          <Route path="/blacklist" element={<BlackList/>} />
           <Route path="/*" element={<Error/>} />
         </Route>
       </Routes>
