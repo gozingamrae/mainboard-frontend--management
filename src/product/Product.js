9eb51390ad32f259633e6719b6a85e0c5edcbd8d
jihi32123 <jihi32123@gmail.com>
2022-09-26 04:22:41 +0900

    Add 상품 조회 페이지 추가

--- src/product/Product.js
+++ src/product/Product.js
@@ -1,6 +1,5 @@
 import style from "./css/Product2.module.css"
 import data from "./data/Product.data.json";
-import TitleBox from "./components/Product/TitleBox";
 import ProductTable from "./components/Product/ProductTable";
 import SearchBox from "./components/Product/SearchBox";
 
@@ -11,7 +10,6 @@ function Product(){
             <div className={style.titleBox}>
                 상품 조회/수정
             </div>
-            <TitleBox/>
             <SearchBox/>
             <ProductTable/>
         </div>
--- src/product/components/Product/SearchBox.js
+++ src/product/components/Product/SearchBox.js
@@ -1,4 +1,4 @@
-import style from "../../css/Product.module.css"
+import style from "../../css/Product2.module.css"
 import Slider from "./slider";
 
 
@@ -8,29 +8,35 @@ function SearchBox(){
     
     return (
         <div className={style.box}>
+            <h1>검색정보</h1>
             <div className = {style.subBox}>
-                <h4>검색어</h4>
+                <h1>검색어</h1>
                 <select>{options.map((option)=>(<option>{option.option}</option>))}</select>
-                <input type="text"/>
+                <input style={{height : "20px"}} type="text"/>
             </div>
             <div className={style.subBox}>
-                <h4>판매상태</h4>
-                <label>판매중</label><input type="checkbox"/><label>판매중지</label><input type="checkbox"/><label>품절</label><input type="checkbox"/>
+                <h1>판매상태</h1>
+                <div>판매중</div><input type="checkbox" style={{margin : "0px 30px 0px 5px"}}/>
+                <div>판매중지</div><input type="checkbox" style={{margin : "0px 30px 0px 5px"}}/>
+                <div>품절</div><input type="checkbox" style={{margin : "0px 30px 0px 5px"}}/>
             </div>
             <div className={style.subBox}>
-                <h4>카테고리</h4>
-                {categorys.map((category)=>(<div><label>{category.name}</label><input type="checkbox"/></div>))}
+                <h1>카테고리</h1>
+                {categorys.map((category)=>(<div><div>{category.name}</div><input type="checkbox" style={{margin : "0px 30px 0px 5px"}}/></div>))}
             </div>   
             <div className={style.subBox}>
-                <h4>대여료</h4>
-                <div className={style.sliderBox}>
+                <h1>대여료</h1>
+                <input type="num" min="0" placeholder="최소값"/>원
+                <div  style={{margin : "0px 10px 0px 10px"}}>~</div>
+                <input type="num" min="0" placeholder="최대값"/>원
+                {/* <div className={style.sliderBox}>
                     <Slider/>
-                </div>
+                </div> */}
             </div>
             <div className={style.subBox}>
-                <h4>기간</h4>
+                <h1>기간</h1>
             </div>
-            <button className={style.submitButton}>검색하기</button>
+            <button className={style.selectButton}>검색하기</button>
         </div>
 
     )
