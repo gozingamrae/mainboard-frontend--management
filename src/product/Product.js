f9b8edbdc6a2e50fb65be8db5701ca534dc58dc6
jihi32123 <jihi32123@gmail.com>
2022-10-05 10:01:14 +0900

    add call api method

--- src/product/Product.js
+++ src/product/Product.js
@@ -2,9 +2,11 @@ import style from "./css/Product2.module.css"
 import data from "./data/Product.data.json";
 import ProductTable from "./components/Product/ProductTable";
 import SearchBox from "./components/Product/SearchBox";
+import {callProductListAPI} from "./apis/ProductAPICalls";
+
 
 function Product(){
-
+   
     return (
         <div className={style.componentBox}>
             <div className={style.titleBox}>
