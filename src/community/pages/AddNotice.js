71605b31933c592a27921cbad708110aa442288d
YougHeeKwon <yh01272002@gmail.com>
2022-10-07 19:07:18 +0900

    Add 공지사항수정 페이지 추가

--- src/community/pages/AddNotice.js
+++ src/community/pages/AddNotice.js
@@ -20,28 +20,11 @@ function AddNotice() {
                 </div>
                 <div className={style.subbox}>
                   <h1>제목</h1>
-                  <input type="text"/>
+                  <input type="text" size="1000"/>
                 </div>
                 <div className={style.subbox}>
                   <h1>공지사항 상세</h1>
                   <input type="text"/>
-                </div>
-                <div className={style.subbox}>
-                  <h1>전시위치</h1>
-                  <select>
-                    <option>전체</option>
-                    <option>웹</option>
-                    <option>모바일</option>
-                  </select>
-                  <input type="checkbox"/> <h1>모든 상품에 공지사항 노출</h1>
-                </div>
-                <div className={style.subbox}>
-                  <h1>전시기간 설정</h1>
-                  <button>설정함</button><button>설정안함</button>
-                </div>
-                <div className={style.subbox}>
-                  <h1>팝업사용</h1>
-                  <button>설정함</button><button>설정안함</button>
                 </div>
                 <div className={style.subbox}>
                   <button>상품 공지사항 등록</button>
