d7d14acf5661233863f6bc725acb62a3cab5206b
jihi32123 <jihi32123@gmail.com>
2022-10-10 08:31:02 +0900

    Refactor 상품 페이지 수정

--- src/product/components/Product/ProductTable.js
+++ src/product/components/Product/ProductTable.js
@@ -13,6 +13,9 @@ function ProductTable(){
     return (
         <div className={style.box}>
                 <div className={style.subTitle}>상품 목록 (총 <span style={{color : "rgb(195, 115, 219)"}}>{products.length}</span>개)</div>
+                <div className={style.subBox}><select><option>판매중</option><option>품절</option><option>판매중지</option></select>
+                    <button>일괄 수정</button>
+                </div>
                 <div>
                     <table className={style.productTable}>
                         <tr><th><input type="checkbox"/></th>
@@ -22,22 +25,25 @@ function ProductTable(){
                         <th>브랜드</th>
                         <th>제조사</th>
                         <th>카테고리</th>
-                        <th style={{width : "5%"}}>수정</th>
-                        <th style={{width : "5%"}}>복사</th>
+                        <th>상태</th>
                         <th style={{width : "5%"}}>옵션보기</th></tr>
                         {/* 테이블 값 출력 */}
                         {products.map((product, index)=>(
                             focusGame == index?
-                                <tr style={{backgroundColor : "#DDDDDD"}}><td><input type="checkbox"/></td>
+                                <tr style={{backgroundColor : "#DDDDDD"}}>
+                                    <td><input type="checkbox"/></td>
                                     <td>{product.상품코드}</td>
                                     <td>{product.상품명}</td>
                                     <td>{product["할인이 적용된 대여료"]}</td>
                                     <td>{product["브랜드"]}</td>
                                     <td>{product["제조사"]}</td>
                                     <td>{product["카테고리"]}</td>
-                                    <td ><button>수정</button></td>
-                                    <td ><button>복사</button></td>
-                                    <td ><div id ={index} onClick={onClickGame}>더보기</div></td>
+                                    <td><table>
+                                        <tr><td><input type="checkbox"/></td><td>최상</td><td>3000원</td><td>판매중</td></tr>
+                                        <tr><td><input type="checkbox"/></td><td>상</td><td>3000원</td><td>판매중</td></tr>
+                                        <tr><td><input type="checkbox"/></td><td>중</td><td>3000원</td><td>판매중</td></tr>
+                                    </table></td>
+                                    <td ><div id ={index} onClick={onClickGame}>상세보기</div></td>
                                 </tr>
                             :
                             <tr><td><input type="checkbox"/></td>
@@ -47,9 +53,12 @@ function ProductTable(){
                                     <td>{product["브랜드"]}</td>
                                     <td>{product["제조사"]}</td>
                                     <td>{product["카테고리"]}</td>
-                                    <td ><button>수정</button></td>
-                                    <td ><button>복사</button></td>
-                                    <td ><div id ={index} onClick={onClickGame}>더보기</div></td>
+                                    <td><table>
+                                        <tr><td><input type="checkbox"/></td><td>최상</td><td>3000원</td><td>판매중</td></tr>
+                                        <tr><td><input type="checkbox"/></td><td>상</td><td>3000원</td><td>판매중</td></tr>
+                                        <tr><td><input type="checkbox"/></td><td>중</td><td>3000원</td><td>판매중</td></tr>
+                                    </table></td>
+                                    <td ><div id ={index} onClick={onClickGame}>상세보기</div></td>
                                 </tr>
                             ))}
                     </table>
