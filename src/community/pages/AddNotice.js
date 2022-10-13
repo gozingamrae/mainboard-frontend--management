de486f9062e1184ce2296574b2280812ce8ddae4
YougHeeKwon <yh01272002@gmail.com>
2022-10-13 20:07:57 +0900

    Refactor 공지사항 css개선

--- src/community/pages/AddNotice.js
+++ src/community/pages/AddNotice.js
@@ -8,7 +8,7 @@ function AddNotice() {
         <div className={style.componentBox}>
             <div className={style.titleBox}>공지사항 등록</div>
             <div className={style.box}>
-                <div className={style.subbox}>
+                <div className={style.sbox}>
                   <h1>분류</h1>
                   <select>
                     <option>일반</option>
@@ -16,17 +16,18 @@ function AddNotice() {
                     <option>배송지연</option>
                     <option>상품</option>
                   </select>
-                  <input type="checkbox"/> <h1>중요 공지사항으로 설정</h1>
+                  <input type="checkbox"/> 
+                  <h1>중요 공지사항으로 설정</h1>
                 </div>
-                <div className={style.subbox}>
+                <div className={style.ntbox}>
                   <h1>제목</h1>
                   <input type="text" size="1000"/>
                 </div>
-                <div className={style.subbox}>
+                <div className={style.nsbox}>
                   <h1>공지사항 상세</h1>
-                  <input type="text"/>
+                  <textarea cols="50" rows="10"/>
                 </div>
-                <div className={style.subbox}>
+                <div className={style.buttonbox}>
                   <button>상품 공지사항 등록</button>
                   <button>취소</button>
                 </div>
--- src/community/pages/ModifyNotice.js
+++ src/community/pages/ModifyNotice.js
@@ -1,35 +1,30 @@
-import style from "../css/community.module.css"
-
-function ModifyNotice() {
-  return(
-    <div className={style.componentBox}>
-    <div className={style.titleBox}>공지사항 등록</div>
-    <div className={style.box}>
-        <div className={style.subbox}>
-          <h1>분류</h1>
-          <select>
-            <option>일반</option>
-            <option>이벤트</option>
-            <option>배송지연</option>
-            <option>상품</option>
-          </select>
-          <input type="checkbox"/> <h1>중요 공지사항으로 설정</h1>
-        </div>
-        <div className={style.subbox}>
-          <h1>제목</h1>
-          <input type="text" size="1000"/>
-        </div>
-        <div className={style.subbox}>
-          <h1>공지사항 상세</h1>
-          <input type="text"/>
-        </div>
-        <div className={style.subbox}>
-          <button>상품 공지사항 수정</button>
-          <button>취소</button>
-        </div>
-    </div>
-</div>
-  )
-}
-
-export default ModifyNotice;
+return (
+  <div className={style.componentBox}>
+      <div className={style.titleBox}>공지사항 등록</div>
+      <div className={style.box}>
+          <div className={style.sbox}>
+            <h1>분류</h1>
+            <select>
+              <option>일반</option>
+              <option>이벤트</option>
+              <option>배송지연</option>
+              <option>상품</option>
+            </select>
+            <input type="checkbox"/> 
+            <h1>중요 공지사항으로 설정</h1>
+          </div>
+          <div className={style.ntbox}>
+            <h1>제목</h1>
+            <input type="text" size="1000"/>
+          </div>
+          <div className={style.nsbox}>
+            <h1>공지사항 상세</h1>
+            <textarea cols="50" rows="10"/>
+          </div>
+          <div className={style.buttonbox}>
+            <button>상품 공지사항 등록</button>
+            <button>취소</button>
+          </div>
+      </div>
+  </div>
+)
